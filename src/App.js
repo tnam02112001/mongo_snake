6025a3ff9eb15372e2e97320524a4514dc6dbb0a
Edward Du <ed.du.email@gmail.com>
2021-02-15 14:50:49 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import * as ReactBootstrap from 'react-bootstrap';
+import { FaArrowCircleLeft } from 'react-icons/fa';
 
-function App() {
+const App = () => {
+  const players = [
+    {rank: "1", name: "Nam", score: 31223},
+    {rank: "2", name: "Michael", score: 29943},
+    {rank: "3", name: "Edward", score: 27181},
+    {rank: "4", name: "Brett", score: 19044},
+    {rank: "5", name: "Will", score: 19021},
+  ]
+
+  const renderPlayer = (player, index) => {
+    return(
+      <tr key={index}>
+        <td>{player.rank}</td>
+        <td>{player.name}</td>
+        <td>{player.score}</td>
+      </tr>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FaArrowCircleLeft className="home-button" />
+      <h1 className="mainText">Leaderboard</h1>
+      <div className="tableContainer">
+        <ReactBootstrap.Table striped hover className="tablecss">
+          <thead>
+            <tr className="colNames">
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody className="dataText">
+            {players.map(renderPlayer)}
+          </tbody>
+        </ReactBootstrap.Table>
+      </div>
     </div>
   );
 }
